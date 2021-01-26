4b8f0efa6b994b9ca4297d2f118df5d735d09b8d
agostinhosamil <agostinhosaml832@gmail.com>
2021-01-26 04:31:10 -0800

    feat(styles): remove the ResetCSS Rule from the main component

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import { Button } from '@chakra-ui/react'
 export default function Home() {
   return (
     <div>
-    	<h1>Hello, World :) ..!</h1>
+    	<h1>Hello, Worlds :) ..!</h1>
   		<Button>
   			Save Datas
   		</Button>
