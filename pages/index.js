3a3c127ea994a4196946da951add3f6244827f9c
Agostinho Sam'l <agostinhosaml832@gmail.com>
2022-04-19 13:07:40 +0100

    feat: update homePage title

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import { Button } from '@chakra-ui/react'
 export default function Home() {
   return (
     <div>
-    	<h1>Hello, Worlds :) ..!</h1>
+    	<h1>Hello, Worlds, Welcome on Taskman :) ..!</h1>
   		<Button>
   			Save Datas
   		</Button>
